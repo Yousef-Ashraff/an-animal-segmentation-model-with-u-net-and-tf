f578ed40b7b8ef4610cb13b09f6630a910093075
Yousef-Ashraff <yousefashraf970@gmail.com>
2024-11-29 17:49:34 +0200

    Update masking.py

--- masking.py
+++ masking.py
@@ -11,13 +11,3 @@ def create_mask(pred_mask, n_h_ori, n_w_ori):
     pred_mask = tf.squeeze(pred_mask, axis=-1)
 
     return pred_mask
-
-# def creat_masked_images(pred_mask):
-#     pred_mask = create_mask(pred_mask)
-#     # Convert tensors to NumPy arrays for OpenCV processing
-#     pred_mask_np = pred_mask.numpy().astype(np.uint8)  # Convert predicted mask to NumPy
-#     # Preprocess the predicted mask: 
-#     # Make the object (label 0) and border (label 2) white (255) and the background (label 1) black (0)
-#     pred_mask_np = (np.where((pred_mask_np == 0) | (pred_mask_np == 2), 255, 0)).astype(np.uint8)
-    
-#     return pred_mask_np
